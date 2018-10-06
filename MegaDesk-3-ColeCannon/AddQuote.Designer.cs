4e714e3e4357870cca0ba794c3df106d42e3a8fc
Cannon-Cole <38596123+Cannon-Cole@users.noreply.github.com>
2018-10-06 00:55:32 -0600

    Validation for height

--- MegaDesk-3-ColeCannon/AddQuote.Designer.cs
+++ MegaDesk-3-ColeCannon/AddQuote.Designer.cs
@@ -30,6 +30,12 @@
         {
             this.AddNewQuoteAcceptBtn = new System.Windows.Forms.Button();
             this.CancelQuoteBtn = new System.Windows.Forms.Button();
+            this.HeightTitle = new System.Windows.Forms.Label();
+            this.WidthTitle = new System.Windows.Forms.Label();
+            this.HeightInputErrorLabel = new System.Windows.Forms.Label();
+            this.WidthInputErrorLabel = new System.Windows.Forms.Label();
+            this.HeightInput = new System.Windows.Forms.TextBox();
+            this.WidthInput = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // AddNewQuoteAcceptBtn
@@ -55,17 +61,81 @@
             this.CancelQuoteBtn.UseVisualStyleBackColor = true;
             this.CancelQuoteBtn.Click += new System.EventHandler(this.CancelQuote_Click);
             // 
+            // HeightTitle
+            // 
+            this.HeightTitle.AutoSize = true;
+            this.HeightTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightTitle.Location = new System.Drawing.Point(197, 57);
+            this.HeightTitle.Name = "HeightTitle";
+            this.HeightTitle.Size = new System.Drawing.Size(56, 20);
+            this.HeightTitle.TabIndex = 5;
+            this.HeightTitle.Text = "Height";
+            // 
+            // WidthTitle
+            // 
+            this.WidthTitle.AutoSize = true;
+            this.WidthTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WidthTitle.Location = new System.Drawing.Point(442, 57);
+            this.WidthTitle.Name = "WidthTitle";
+            this.WidthTitle.Size = new System.Drawing.Size(50, 20);
+            this.WidthTitle.TabIndex = 6;
+            this.WidthTitle.Text = "Width";
+            // 
+            // HeightInputErrorLabel
+            // 
+            this.HeightInputErrorLabel.AutoSize = true;
+            this.HeightInputErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.HeightInputErrorLabel.Location = new System.Drawing.Point(198, 133);
+            this.HeightInputErrorLabel.Name = "HeightInputErrorLabel";
+            this.HeightInputErrorLabel.Size = new System.Drawing.Size(0, 13);
+            this.HeightInputErrorLabel.TabIndex = 7;
+            this.HeightInputErrorLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // WidthInputErrorLabel
+            // 
+            this.WidthInputErrorLabel.AutoSize = true;
+            this.WidthInputErrorLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.WidthInputErrorLabel.ForeColor = System.Drawing.Color.Red;
+            this.WidthInputErrorLabel.Location = new System.Drawing.Point(443, 133);
+            this.WidthInputErrorLabel.Name = "WidthInputErrorLabel";
+            this.WidthInputErrorLabel.Size = new System.Drawing.Size(0, 13);
+            this.WidthInputErrorLabel.TabIndex = 8;
+            // 
+            // HeightInput
+            // 
+            this.HeightInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HeightInput.Location = new System.Drawing.Point(201, 80);
+            this.HeightInput.Name = "HeightInput";
+            this.HeightInput.Size = new System.Drawing.Size(100, 38);
+            this.HeightInput.TabIndex = 9;
+            this.HeightInput.Validating += new System.ComponentModel.CancelEventHandler(this.HeightInputValidation);
+            // 
+            // WidthInput
+            // 
+            this.WidthInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.WidthInput.Location = new System.Drawing.Point(446, 80);
+            this.WidthInput.Name = "WidthInput";
+            this.WidthInput.Size = new System.Drawing.Size(100, 38);
+            this.WidthInput.TabIndex = 10;
+            // 
             // AddQuote
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.WidthInput);
+            this.Controls.Add(this.HeightInput);
+            this.Controls.Add(this.WidthInputErrorLabel);
+            this.Controls.Add(this.HeightInputErrorLabel);
+            this.Controls.Add(this.WidthTitle);
+            this.Controls.Add(this.HeightTitle);
             this.Controls.Add(this.CancelQuoteBtn);
             this.Controls.Add(this.AddNewQuoteAcceptBtn);
             this.Name = "AddQuote";
             this.Text = "AddQuote";
             this.Load += new System.EventHandler(this.AddQuote_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -73,5 +143,11 @@
 
         private System.Windows.Forms.Button AddNewQuoteAcceptBtn;
         private System.Windows.Forms.Button CancelQuoteBtn;
+        private System.Windows.Forms.Label HeightTitle;
+        private System.Windows.Forms.Label WidthTitle;
+        private System.Windows.Forms.Label HeightInputErrorLabel;
+        private System.Windows.Forms.Label WidthInputErrorLabel;
+        private System.Windows.Forms.TextBox HeightInput;
+        private System.Windows.Forms.TextBox WidthInput;
     }
 }
--- MegaDesk-3-ColeCannon/AddQuote.cs
+++ MegaDesk-3-ColeCannon/AddQuote.cs
@@ -33,5 +33,48 @@ namespace MegaDesk_3_ColeCannon
             mainMenu.Show();
             Close();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void HeightInputValidation(object sender, CancelEventArgs e)
+        {
+            string errorMessage;
+            int height;
+
+            try
+            { 
+            height = System.Convert.ToInt32(HeightInput.Text);
+            }
+            catch(FormatException)
+            {
+                HeightInputErrorLabel.Text = "Not integer";
+                height = 0;
+            }
+            catch (OverflowException)
+            {
+                HeightInputErrorLabel.Text = "Too big";
+                height = 0;
+            }
+
+            if (height > 48)
+            {
+                errorMessage = "Height must be less than or equal to 48";
+                HeightInputErrorLabel.Text = errorMessage;
+                e.Cancel = true;
+            }
+            else if (height < 12)
+            {
+                errorMessage = "Height must be greater than or equal to 12";
+                HeightInputErrorLabel.Text = errorMessage;
+                e.Cancel = true;
+            }
+            else
+            {
+                HeightInputErrorLabel.Text = "";
+            }
+        }
     }
 }
